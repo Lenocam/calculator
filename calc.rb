08eea35298a639f31440480d4cff7420b13dcb88
Reed S. MacConnell <learn22ruby@gmail.com>
2013-08-07 11:45:44 -0400

    Still trying to clean up my calculator code

--- calc.rb
+++ calc.rb
@@ -1,41 +1,34 @@
 def say(msg)
-	puts "=>>#{msg}"
-end	
-say('This is a simple calculator.')
-say('That does basic calculations.')
-say('Addition, Subtraction, Division, and Multipliaction of two numbers.')
-say('Enter your fist number to begin:')
+  puts "=>> #{msg}"
+end
+
+say 'enter your fist number'
 number_1 = gets.chomp
 
-say('Now your second number:')
+say('enter your second number')
 number_2 = gets.chomp
+ 
 
-directions = 'Type "1" to add, "2" to subtract, "3 to divide, "4" to multiply'
-say(directions)
 
-while operation.to_i > 4 || choice.to_i < 1
-	say("You need to pick a operation using the numbers 1 through 4.")	
-	operation = gets.chomp
-	mark = 'This is the result of your calulation:'
-		if operation == '1'
-	add = number_1.to_i + number_2.to_i
-		say(mark) 
-		say(add)
-			break
-		elsif operation == '2'
-	subt = number_1.to_i - number_2.to_i
-		says(mark)
-		says(subt)
-			break
-		elsif operation == '3'
-	div = number_1.to_f / number_2.to_f
-		say(mark)
-		say(div)
-		break
-		elsif operation == '4'
-	mult = number_1.to_i * number_2.to_i
-		says(mark)
-		says(mult)
-		break
-	end
+while true
+  say('Type "1" to add, "2" to subtract, "3 to divide, "4" to multiply')  
+  operation = gets.chomp.to_s
+
+    if operation == '1'
+  outcome = number_1.to_i + number_2.to_i
+    say(outcome)
+      break
+    elsif operation == '2'
+  outcome = number_1.to_i - number_2.to_i
+    say(outcome)
+      break
+    elsif operation == '3'
+  outcome = number_1.to_f / number_2.to_f
+    say(outcome)
+    break
+    elsif operation == '4'
+  outcome = number_1.to_i * number_2.to_i
+    say(outcome)
+    break
+  end
 end
